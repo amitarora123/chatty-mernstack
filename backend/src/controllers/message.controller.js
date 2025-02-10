019cdaac675266251a90c75daeedd058a9a84576
Amit <baghlaamit06@gmail.com>
2025-02-10 17:53:54 +0530

    add multer middleware for file uploads; implement message fetching and sending functionality

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,9 +1,15 @@
 import User from "../models/users.model.js";
 import ApiError from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
-const getUsersForSidebar = async (req, res, next) => {
+import Message from "../models/message.model.js";
+import cloudinary from "../lib/cloudinary.js";
+import { asyncHandler } from "../utils/AsyncHandler.js";
+const getUsersForSidebar = asyncHandler(async (req, res) => {
   try {
-    const users = await User.find().select("-password -email");
+    const loggedInUserId = req.user._id;
+    const users = User.find({ _id: { $ne: loggedInUserId } }).select(
+      "-password "
+    );
 
     return res
       .status(200)
@@ -13,8 +19,63 @@ const getUsersForSidebar = async (req, res, next) => {
       "message.controller.js getUsersForSidebar error ",
       error.message
     );
+    throw new ApiError(500, "Internal Server Error");
+  }
+});
+
+const getMessages = asyncHandler(async (req, res) => {
+  try {
+    const { id: userToChatId } = req.params;
+    const myId = req.user._id;
+
+    const messages = await Message.find({
+      $or: [
+        { senderId: myId, receiverId: userToChatId },
+        { senderId: userToChatId, receiverId: myId },
+      ],
+    });
+
+    return res
+      .status(200)
+      .json(new ApiResponse(200, messages, "messages fetched successfully"));
+  } catch (error) {
+    console.log(
+      "message.controller.js getUsersForSidebar error ",
+      error.message
+    );
+    throw new ApiError(500, "Internal Server Error", [error]);
+  }
+});
+
+const sendMessage = async (req, res) => {
+  try {
+    const { text } = req.body;
+    const image = req.file.path;
+    const { id: receiverId } = req.params;
+    const senderId = req.user._id;
+
+    let imageUrl;
+    
+    if (image) {
+      const uploadedResponse = await cloudinary.uploader.upload(image);
+      imageUrl = uploadedResponse.secure_url;
+    }
+
+    const newMessage = new Message({
+      senderId,
+      receiverId,
+      text,
+      image: imageUrl || "",
+    });
+
+    await newMessage.save();
+
+    return res
+      .status(200)
+      .json(new ApiResponse(200, newMessage, "message sent successfully"));
+  } catch (error) {
+    console.log("message.controller.js sendmessage error ", error.message);
     next(error);
   }
 };
-
-export { getUsersForSidebar };
+export { getUsersForSidebar, getMessages, sendMessage };
--- backend/src/routes/message.routes.js
+++ backend/src/routes/message.routes.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
-import {getUsersForSidebar} from "../controllers/message.controller.js"
+import {getUsersForSidebar, getMessages, sendMessage} from "../controllers/message.controller.js"
+import { upload } from "../middleware/multer.middleware.js";
 
 const router = Router();
 
-router.get("/users", protectRoute, getUsersForSidebar)
+router.use(protectRoute)
+router.get("/users", getUsersForSidebar)
+router.get("/:id", getMessages)
+router.post("/send/:id",upload.single("image"), sendMessage)
 
 export default router;
